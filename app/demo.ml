9ab7952d581158b10e33a60f8f677053d740cff0
Philippe Veber <philippe.veber@gmail.com>
2019-03-31 23:43:41 +0200

    demo: added vstack

--- app/demo.ml
+++ app/demo.ml
@@ -1,8 +1,16 @@
 open Core
+open Gg
 open Croquis
 
 let single_letter () =
   Picture.text ~x:5. ~y:0. "croquis"
+
+let vstack () =
+  let open Picture in
+  vstack [
+    rect ~fill:Color.green ~xmin:5. ~xmax:30. ~ymin:0. ~ymax:5. () ;
+    text ~x:5. ~y:0. "croquis" ;
+  ]
 
 let demo name f =
   name,
@@ -12,6 +20,7 @@ let demo name f =
   
 let command = Command.group ~summary:"Demo application for croquis" [
     demo "single-letter" single_letter ;
+    demo "vstack" vstack ;
   ]
 
 let () = Command.run command
